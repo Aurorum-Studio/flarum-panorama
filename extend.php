c1d5c80a83d96e8cd7d4bfef8f88c1da87447564
Aurorum <88573201+Aurorum-Studio@users.noreply.github.com>
2023-03-14 11:15:53 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -21,7 +21,39 @@ return [
     ->configure(function (Configurator $config) {
          $config->BBCodes->addCustom(
            '[pan="{URL}"]',
-           'require("./a.html")'
+           '<div>
+		          <meta name="viewport" content="initial-scale=1.0" />
+		          <script src="../three.min.js"></script>
+		          <script src="../photo-sphere-viewer.min.js"></script>
+		          <style>
+			           html, body {
+				         margin: 0;
+				         width: 100%;
+				         height: 100%;
+				         overflow: hidden;
+			        }
+
+			           #container {
+				          width: 100%;
+				          height: 100%;
+			        }
+		           </style>
+		     </div>
+
+		       <div id="container"></div>
+
+		       <script>
+			     var div = document.getElementById("container");
+			     var PSV = new PhotoSphereViewer({
+					 panorama: "{URL}",
+					 container: div,
+					 time_anim: 3000,
+					 navbar: true,
+					 navbar_style: {
+						 backgroundColor: "rgba(58, 67, 77, 0.7)"
+					 },
+				  });
+		      </script>'
         );
     })
 ];
